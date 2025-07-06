8e08adc3cdc8b3cea674c8c7006b821107d969cd
Mirror-Prismals <PurpleRed80@proton.me>
2025-07-05 23:43:31 -0400

    Add LCM calculation program

---
+++ lcm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+static unsigned int gcd(unsigned int a, unsigned int b) {
+    while (b != 0u) {
+        unsigned int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+unsigned int lcm(unsigned int a, unsigned int b) {
+    if (a == 0u || b == 0u) {
+        return 0u;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+int main(void) {
+    unsigned int a, b;
+    printf("Enter two non-negative integers: ");
+    if (scanf("%u %u", &a, &b) != 2) {
+        fprintf(stderr, "Invalid input.\n");
+        return 1;
+    }
+    printf("lcm(%u, %u) = %u\n", a, b, lcm(a, b));
+    return 0;
+}
