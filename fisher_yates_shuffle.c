a49bbf8c216093f4e271822b8c92aa6101c1bb96
Mirror-Prismals <PurpleRed80@proton.me>
2025-07-06 00:07:51 -0400

    Add Fisher-Yates shuffle

---
+++ fisher_yates_shuffle.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static void shuffle(int *arr, int n) {
+    for (int i = n - 1; i > 0; --i) {
+        int j = rand() % (i + 1);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+int main(void) {
+    int n;
+    printf("Enter number of integers: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        fprintf(stderr, "Invalid input.\n");
+        return 1;
+    }
+    int *arr = malloc(n * sizeof(int));
+    if (!arr) {
+        perror("malloc");
+        return 1;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (scanf("%d", &arr[i]) != 1) {
+            fprintf(stderr, "Invalid input.\n");
+            free(arr);
+            return 1;
+        }
+    }
+    srand((unsigned)time(NULL));
+    shuffle(arr, n);
+    printf("Shuffled integers:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d\n", arr[i]);
+    }
+    free(arr);
+    return 0;
+}
